24da5b5558dc858dd1f768be514bd4ebf06dfe7a
nzkarit <nzkarit@users.noreply.github.com>
2018-08-19 12:25:58 +1200

    SQL for the site

---
+++ sql_for_web_site/stop_status_cancelled.sql
@@ -0,0 +1,12 @@
+SELECT date, serviceid, sms, departurestatus, COUNT(*) AS Number
+  FROM stop_departures_details sd1
+  WHERE timestamp =
+    (SELECT MAX(timestamp)
+        FROM stop_departures_details sd2
+        WHERE sd1.aimeddeparture = sd2.aimeddeparture
+            AND sd1.serviceid = sd2.serviceid
+	    AND sd1.sms = sd2.sms
+            AND sd2.departurestatus = 'cancelled')
+    AND sd1.departurestatus = 'cancelled'
+    GROUP BY date, serviceid, sms
+    ORDER BY date, serviceid, sms;
